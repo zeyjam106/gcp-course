5b0797cd9b8422f095c77fe90ebb0cb29727c5e5
zeyjam106 <54830058+zeyjam106@users.noreply.github.com>
2022-09-23 17:36:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//-----------------------------------------------------------------------------
+// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
+// Copyleft (c) by Denis Astahov
+//-----------------------------------------------------------------------------
+
+exports.helloJAM = (req, res) => {
+  const message="<font color='blue'>СloudFunction of Jamil Zeynalov!</font><br><b>App Version 5.5</b>";
+  res.status(200).send(message);
+};

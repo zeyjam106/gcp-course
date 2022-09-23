479f4d628c0d45dcf7d26f670bb1f35431a11478
zeyjam106 <54830058+zeyjam106@users.noreply.github.com>
2022-09-23 17:47:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloJAM = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Jamil Zeynalov!</font><br><b>App Version 5.5</b>";
+  const message="<font color='blue'>СloudFunction of Jamka Zeynalov!</font><br><b>App Version 5.5</b>";
   res.status(200).send(message);
 };
